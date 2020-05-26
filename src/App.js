97c9698cb71dad64363fa3e4676b8f1627d06793
Mohamed Elgazzar <thisisgazzar@gmail.com>
2020-05-26 20:31:21 +0200

    optimization

--- src/App.js
+++ src/App.js
@@ -3,18 +3,17 @@ import TodoList from './components/todoList';
 import Input from './components/input';
 import './css/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App container">
-        <div id="inputWrapper">
-          <Input />
-        </div>
-        <div id="todoWrapper">
-          <TodoList />
-        </div>
+function App() {
+  return (
+    <div className="App container">
+      <div id="inputWrapper">
+        <Input />
       </div>
-    );
-  }
+      <div id="todoWrapper">
+        <TodoList />
+      </div>
+    </div>
+  );
 }
+
 export default App;
